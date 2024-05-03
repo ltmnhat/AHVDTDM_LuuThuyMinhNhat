664e5b3c85eaf6fdc78393bcce8afdcdf019e20f
Luu Thuy Minh Nhat <minhnhat240820032019@gmail.com>
2024-05-03 13:34:57 +0700

    add Bai1

---
+++ Bai1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int isMultipleOf7(int num) {
+    return (num % 7 == 0);
+}
+
+int main() {
+    printf("Cac so nguyen co 2 chu so va la boi cua 7 la:\n");
+
+    for (int i = 10; i < 100; i++) {
+        if (isMultipleOf7(i)) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
+
